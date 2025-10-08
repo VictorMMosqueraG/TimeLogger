b481bc7197b29c6f235767f4b3e38a4530cc8d2f
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-10-08 18:52:32 -0500

    configuration(firebase): Configuracion del farebase

---
+++ src/environments/firebase.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDtVqsjrjb6mbvUSIPpfXEdy6ALeCMjIg4",
+    authDomain: "pruebaemp-85d79.firebaseapp.com",
+    projectId: "pruebaemp-85d79",
+    storageBucket: "pruebaemp-85d79.firebasestorage.app",
+    messagingSenderId: "263702123868",
+    appId: "1:263702123868:web:177494876a4ae7f0a99d99",
+    measurementId: "G-Y48W7TL0KW"
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/firebase';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
+}).catch(err => console.error(err));
