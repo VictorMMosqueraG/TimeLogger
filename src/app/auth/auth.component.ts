3305ddb18e91006044099ea217d617253483297c
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-10-08 19:20:51 -0500

    refactor(auth): Mejora de la vista y codigo

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -22,15 +22,22 @@ import { AsyncPipe, NgIf } from '@angular/common';
 })
 export class AuthComponent {
 
+  //Propiedades para los campos
   email = '';
   password = '';
-  registered = false;
-  user$: Observable<User | null>;
+
+  registered = false;// Flag
+  loginError: string | null = null;//Control de errores
+  user$: Observable<User | null>;//Observable que obtiene el user
 
   constructor(private auth: Auth) {
     this.user$ = authState(this.auth);
   }
 
+  //**
+  // Registra el usuario,
+  // con la opcion del "crear cuenta"
+  //  */
   async registrar() {
     await createUserWithEmailAndPassword(this.auth, this.email, this.password);
     await signOut(this.auth);
@@ -38,17 +45,38 @@ export class AuthComponent {
     this.password = '';
   }
 
+  //***
+  // Obtiene los datos,
+  // de ser correctos permite el acceso
+  // con la opcion de "ingresar"
+  //  */
   async ingresar() {
-    await signInWithEmailAndPassword(this.auth, this.email, this.password);
+    this.loginError = null;
+    try {
+      await signInWithEmailAndPassword(this.auth, this.email, this.password);
+    } catch (err: any) {
+      this.loginError = "Credenciales Invalidas. Porfavor intentelo nuevamente";//COMEBACK: revisar como no hardcodear esto
+    }
   }
 
+  //***
+  // Se conecta con
+  // la autenticacion de googler,
+  // para crear cuenta e ingresar
+  //  */
   async ingresarConGoogle() {
     await signInWithPopup(this.auth, new GoogleAuthProvider());
   }
 
+  //***
+  // Limpia los campos
+  // y cierra la sesion
+  //  */
   async salir() {
     await signOut(this.auth);
     this.email = '';
     this.password = '';
   }
+
+
 }
